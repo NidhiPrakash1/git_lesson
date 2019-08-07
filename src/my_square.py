e788f177704038ce12a5bda8013f25e1314a7783
NidhiPrakash1 <53871488+NidhiPrakash1@users.noreply.github.com>
2019-08-07 13:30:06 -0400

    x=> z and square? 22

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
-def my_square(x):
+def my_square(z):
 	""" takes a valuea nd returns the squared value, in this case the value is 3.
 	uses the ** operator
 	"""
-	return(x ** 2)
+	return(z * 2)
 
-print(my_square(5))
+print(my_square(22))
